68a073238a35b8aae64c2b5c2231f64338e10d5d
reyriyansanjaya <reyriyansanjaya@gmail.com>
2025-07-19 01:12:47 +0700

    update

---
+++ program/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.19.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\kuliah\\skripsi\\program" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.19.3-stable\\flutter"
+  "PROJECT_DIR=E:\\kuliah\\skripsi\\program"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.19.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\kuliah\\skripsi\\program\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\kuliah\\skripsi\\program"
+  "FLUTTER_TARGET=E:\\kuliah\\skripsi\\program\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\kuliah\\skripsi\\program\\.dart_tool\\package_config.json"
+)
